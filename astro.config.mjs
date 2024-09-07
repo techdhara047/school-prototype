5517aff1ff0f25fa158012004f804adc5a92ba18
techdhara047 <techdhara047@gmail.com>
2024-09-07 12:26:01 +0530

    removed robots.txt.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()],
+  site:'https://school-prototype.netlify.app/',
+  integrations: [tailwind(), icon(), react(), sitemap()]
 });
